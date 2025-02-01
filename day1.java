6624aec1a7680f86480353d2b9fa26663639b6c1
narendramongam <mongamnarendra@gmail.com>
2025-02-01 21:58:25 +0530

    started course

---
+++ day1.java
@@ -0,0 +1,11 @@
+package techwing;
+
+import java.util.Scanner;
+
+public class day1 {
+    static byte a=127;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int id=sc.nextInt();
+    }
+}
